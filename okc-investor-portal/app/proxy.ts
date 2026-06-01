57f32b765dc9ce0beb31140c048413f6ec7263c3
Yinuo <s10269277@connect.np.edu.sg>
2026-06-01 22:39:54 +0800

    update middleware + clerk testing

--- okc-investor-portal/app/proxy.ts
+++
@@ -1,8 +0,0 @@
-export { auth as proxy } from "@/lib/auth";
-
-export const config = {
-  matcher: [
-    // Match all routes except static files and API routes
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-  ],
-};
