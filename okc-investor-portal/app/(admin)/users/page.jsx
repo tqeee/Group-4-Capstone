ebd59936abd1c83d2aee9c7c322818acbae1673c
Yinuo <s10269277@connect.np.edu.sg>
2026-07-08 04:05:25 +0800

    remove redundant pages

--- okc-investor-portal/app/(admin)/users/page.jsx
+++
@@ -1,8 +0,0 @@
-import { listUsers } from './actions';
-import UsersClient from './UsersClient';
-
-export default async function UsersPage() {
-  const result = await listUsers();
-
-  return <UsersClient users={result.users ?? []} loadError={result.error ?? null} />;
-}
